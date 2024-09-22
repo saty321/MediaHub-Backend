14b6c4157ad03b48a21daaf3e8e5da18f130e44b
saty321 <ajsatyamaj123@gmail.com>
2024-09-23 01:43:33 +0530

    add user management endpoints

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,13 @@
 import { Router } from "express";
 const router = Router();
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+ } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,5 +29,7 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
 
 export {router}
